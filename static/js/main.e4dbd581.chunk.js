9f66d7bf4ea22dd9792a32cfa20810d72bcc275a
Sardorbek Makhamadaliev <smakhamadaliev8@gmail.com>
2022-06-06 13:36:41 +0200

    Updates

---
+++ static/js/main.e4dbd581.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact_static-list-of-posts"]=this["webpackJsonpreact_static-list-of-posts"]||[]).push([[0],{10:function(t,s,c){"use strict";c.r(s);var e=c(3),i=c.n(e),n=(c(1),c(8),c(0)),o=function(){return Object(n.jsx)("div",{className:"App",children:Object(n.jsx)("h1",{children:"Static list of posts"})})};i.a.render(Object(n.jsx)(o,{}),document.getElementById("root"))},8:function(t,s,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.e4dbd581.chunk.js.map
